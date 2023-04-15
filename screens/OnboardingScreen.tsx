02f3bb67f93c883572c7e1d14f99ed7f79a5fadb
collo <mutaic84@gmail.com>
2023-04-15 11:38:02 +0300

    :hammer: create basic onboarding layout

---
+++ screens/OnboardingScreen.tsx
@@ -0,0 +1,49 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Onboarding from "react-native-onboarding-swiper";
+
+type Props = {};
+
+const OnboardingScreen = (props: Props) => {
+  return (
+    <Onboarding
+      pages={[
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={require("../assets/6861484.jpg")}
+              className="w-72 h-72 object-contain"
+            />
+          ),
+          title: "Onboarding",
+          subtitle: "Done with react native onboading swiper",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={require("../assets/5124556.jpg")}
+              className="w-72 h-72 object-contain"
+            />
+          ),
+          title: "Onboarding",
+          subtitle: "Done with react native onboading swiper",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={require("../assets/4590393.jpg")}
+              className="w-72 h-72 object-contain"
+            />
+          ),
+          title: "Onboarding",
+          subtitle: "Done with react native onboading swiper",
+        },
+      ]}
+    />
+  );
+};
+
+export default OnboardingScreen;
